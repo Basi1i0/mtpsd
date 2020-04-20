a5ddfc41986d2c73670304f8577ba509512c3204
Basil <OldBasilio@gmail.com>
2020-04-20 00:06:28 -0700

    header fix

--- include/ccomplex_functions.h
+++ include/ccomplex_functions.h
@@ -1,11 +1,14 @@
 #ifndef CCOMPLEX_FUNCTIONS_H
 #define CCOMPLEX_FUNCTIONS_H
 #include "fftw3.h"
+#include <complex>
 
 
 double cabs(const fftw_complex z);
 
 
 double cabs(const double z);
+
+double cabs(std::complex<double> z);
 
 #endif // !CCOMPLEX_FUNCTIONS_H
